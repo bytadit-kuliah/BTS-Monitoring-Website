6d4561f11da90304130ba1fd1b2ea1ed0b3982e1
magezter-stdnt <christopheraaronw@student.uns.ac.id>
2022-06-21 21:41:18 +0700

    update admin surveys

--- Apl Laravel/monitorBts/resources/views/dashboard/admin/survey/index.blade.php
+++ Apl Laravel/monitorBts/resources/views/dashboard/admin/survey/index.blade.php
@@ -13,7 +13,7 @@
                 <h6 class="card-title">topik survey</h6>
                 <p class="card-text">Deskripsi survey deskripsi survey deskripsi survey deskripsi survey deskripsi survey deskripsi survey deskripsi survey.</p>
                 <div class="d-flex" style="justify-content: flex-end;">
-                    <a class="text-black-50" href="/admin/edit-survey" class="card-link">Edit</a>
+                    <a class="text-black-50" href="/dashboard/show-survey" class="card-link">Lihat</a>
                     <!-- <a href="#" class="card-link">Edit Jawaban</a> -->
                 </div>
             </div>
@@ -24,7 +24,7 @@
                 <h6 class="card-title">topik survey</h6>
                 <p class="card-text">Deskripsi survey deskripsi survey deskripsi survey deskripsi survey deskripsi survey deskripsi survey deskripsi survey.</p>
                 <div class="d-flex" style="justify-content: flex-end;">
-                    <a class="text-black-50" href="/admin/edit-survey" class="card-link">Edit</a>
+                    <a class="text-black-50" href="/dashboard/show-survey" class="card-link">Lihat</a>
                     <!-- <a href="#" class="card-link">Edit Jawaban</a> -->
                 </div>
             </div>
@@ -35,7 +35,7 @@
                 <h6 class="card-title">topik survey</h6>
                 <p class="card-text">Deskripsi survey deskripsi survey deskripsi survey deskripsi survey deskripsi survey deskripsi survey deskripsi survey.</p>
                 <div class="d-flex" style="justify-content: flex-end;">
-                    <a class="text-black-50" href="/admin/edit-survey" class="card-link">Edit</a>
+                    <a class="text-black-50" href="/dashboard/show-survey" class="card-link">Lihat</a>
                     <!-- <a href="#" class="card-link">Edit Jawaban</a> -->
                 </div>
             </div>
@@ -46,7 +46,7 @@
                 <h6 class="card-title">topik survey</h6>
                 <p class="card-text">Deskripsi survey deskripsi survey deskripsi survey deskripsi survey deskripsi survey deskripsi survey deskripsi survey.</p>
                 <div class="d-flex" style="justify-content: flex-end;">
-                    <a class="text-black-50" href="/admin/edit-survey" class="card-link">Edit</a>
+                    <a class="text-black-50" href="/dashboard/show-survey" class="card-link">Lihat</a>
                     <!-- <a href="#" class="card-link">Edit Jawaban</a> -->
                 </div>
             </div>
@@ -57,7 +57,7 @@
                 <h6 class="card-title">topik survey</h6>
                 <p class="card-text">Deskripsi survey deskripsi survey deskripsi survey deskripsi survey deskripsi survey deskripsi survey deskripsi survey.</p>
                 <div class="d-flex" style="justify-content: flex-end;">
-                    <a class="text-black-50" href="/admin/edit-survey" class="card-link">Edit</a>
+                    <a class="text-black-50" href="/dashboard/show-survey" class="card-link">Lihat</a>
                     <!-- <a href="#" class="card-link">Edit Jawaban</a> -->
                 </div>
             </div>
@@ -65,7 +65,7 @@
         <div class="card text-black bg-light border-3 add-card survey-card" style="width: 18rem; margin:10px">
                 <h3 class="card-header">Tambah Survey</h3>
                 <div class="card-body d-flex" style="justify-content: center;">
-                    <a class="text-black" style="font-size: 6em; align-self: center;" href="newSurvey.html" class="card-link">+</a>
+                    <a class="text-black" style="font-size: 6em; align-self: center;" href="/dashboard/create-survey" class="card-link">+</a>
                 </div>
         </div>
     </div>
