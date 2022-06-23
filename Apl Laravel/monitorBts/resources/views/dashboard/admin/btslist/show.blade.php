2434bebd72399ac5e5d03a0c3e9df13800ae883a
magezter-stdnt <christopheraaronw@student.uns.ac.id>
2022-06-23 23:14:25 +0700

    update admin dashboard - btslist

--- Apl Laravel/monitorBts/resources/views/dashboard/admin/btslist/show.blade.php
+++ Apl Laravel/monitorBts/resources/views/dashboard/admin/btslist/show.blade.php
@@ -18,65 +18,112 @@
            <br>
 
            @if($btsphotos)
-            @foreach ($btsphotos as $btsphoto)
-                <div style="max-height: 200px; max-width:400px; overflow:hidden; display:inline-block;">
-                    <img src="{{ asset('storage/' . $btsphoto->url) }}" alt="{{ $btslist->btstype->type }}" class="img-fluid mt-3">
+                <div id="btsPhotosCarousel" class="carousel slide text-dark" data-bs-ride="true">
+                    <div class="carousel-indicators">
+                        @foreach ($btsphotos as $btsphoto)
+                            @if ($loop->iteration == 0)
+                                <button type="button" data-bs-target="#btsPhotosCarousel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                            @else
+                                <button type="button" data-bs-target="#btsPhotosCarousel" data-bs-slide-to="{{$loop->iteration}}" aria-label="Slide {{($loop->iteration)+1}}"></button>                            
+                            @endif
+                        @endforeach
+                    </div>
+                    <div class="carousel-inner">
+                        @foreach ($btsphotos as $btsphoto)
+                            <div class="carousel-item active">
+                                <img src="{{ asset('storage/' . $btsphoto->url) }}" alt="{{ $btslist->btstype->type }}" class="w-75 rounded-5 img-fluid mb-5 mt-5">
+                            </div>
+                        @endforeach
+                    </div>
+                    <button class="carousel-control-prev" type="button" data-bs-target="#btsPhotosCarousel" data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden=""></span>
+                        <span class="visually-hidden">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-bs-target="#btsPhotosCarousel" data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden=""></span>
+                        <span class="visually-hidden">Next</span>
+                    </button>
                 </div>
-            @endforeach
            @else
-               <img src="https://source.unsplash.com/400x200?{{ $btslist->btstype->type }}" alt="{{ $btslist->btstype->type }}" class="img-fluid mt-3">
+               <img src="https://source.unsplash.com/400x200?{{ $btslist->btstype->type }}" alt="{{ $btslist->btstype->type }}" class="w-75 rounded-5 img-fluid mb-5 mt-3">
            @endif
-           <ul>
-                {{-- <li>
-                    <h5 class="mb-2">Nama : {{ $btslist->nama }}</h5>
-                </li> --}}
-                <li>
-                    <h5 class="mb-2">Tipe BTS : {{ $btslist->btstype->type }}</h5>
-                </li>
-                <li>
-                    <h5 class="mb-2">Provider :</h5>
-                    @if($btslist->provider)
-                        <ul>
-                            @foreach($btslist->provider as $providerBtslist)
-                                <li>{{ $providerBtslist->nama }}</li>
-                            @endforeach
-                        </ul>
-                    @else
-                        <p>None</p>
-                    @endif
-                </li>
-                <li>
-                    <h5 class="mb-2">Alamat : {{ $btslist->lokasi }}</h5>
-                </li>
-                <li>
-                    <h5 class="mb-2">Kelurahan : {{ $btslist->village->nama }}</h5>
-                </li>
-                <li>
-                    <h5 class="mb-2">Kecamatan : {{ $btslist->village->kecamatan->nama }}</h5>
-                </li>
-                <li>
-                    <h5 class="mb-2">Genset : {{ $btslist->genset }}</h5>
-                </li>
-                <li>
-                    <h5 class="mb-2">Tembok Batas : {{ $btslist->tembokBatas }}</h5>
-                </li>
-                <li>
-                    <h5 class="mb-2">Panjang Tanah : {{ $btslist->panjangTanah }}</h5>
-                </li>
-                <li>
-                    <h5 class="mb-2">Lebar Tanah : {{ $btslist->lebarTanah }}</h5>
-                </li>
-                <li>
-                    <h5 class="mb-2">Tinggi Tower : {{ $btslist->tinggiTower }}</h5>
-                </li>
-                <li>
-                    <h5 class="mb-2">Latitude : {{ $btslist->latitude }}</h5>
-                </li>
-                <li>
-                    <h5 class="mb-2">Longitude : {{ $btslist->longitude }}</h5>
-                </li>
-            </ul>
 
+            <table class="table ">
+                <thead class="table-success ">
+                    <th style='border-top-left-radius:1rem'>
+                        Keterangan
+                    </th>
+                    <th style='border-top-right-radius:1rem'>
+                        Nilai
+                    </th>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td class='table-light w-25'>Nama</td>
+                        <td> {{ $btslist->nama }} </td>
+                    </tr>
+                    <tr>
+                        <td class='table-light'>Tipe BTS</td>
+                        <td> {{ $btslist->btstype->type }} </td>
+                    </tr>
+                    <tr>
+                        <td class='table-light'>Provider</td>
+                        <td> 
+                            @if($btslist->provider)
+                                @foreach($btslist->provider as $providerBtslist)
+                                    @if ($loop->iteration==0)
+                                        {{ $providerBtslist->nama }}
+                                    @else
+                                        {{ ','.$providerBtslist->nama }}
+                                    @endif                                
+                                @endforeach
+                            @else
+                                -
+                            @endif
+                        </td>
+                    </tr>
+                    <tr>
+                        <td class='table-light'>Alamat</td>
+                        <td> {{ $btslist->lokasi }} </td>
+                    </tr>
+                    <tr>
+                        <td class='table-light'>Kelurahan</td>
+                        <td> {{ $btslist->village->nama }} </td>
+                    </tr>
+                    <tr>
+                        <td class='table-light'>Kecamatan</td>
+                        <td> {{ $btslist->village->kecamatan->nama }} </td>
+                    </tr>
+                    <tr>
+                        <td class='table-light'>Genset</td>
+                        <td> {{ $btslist->genset }} </td>
+                    </tr>
+                    <tr>
+                        <td class='table-light'>Tembok Batas</td>
+                        <td>  {{ $btslist->tembokBatas }} </td>
+                    </tr>
+                    <tr>
+                        <td class='table-light'>Panjang Tanah</td>
+                        <td> {{ $btslist->panjangTanah }} </td>
+                    </tr>
+                    <tr>
+                        <td class='table-light'>Lebar Tanah</td>
+                        <td> {{ $btslist->lebarTanah }} </td>
+                    </tr>
+                    <tr>
+                        <td class='table-light'>Tinggi Tower</td>
+                        <td> {{ $btslist->tinggiTower }} </td>
+                    </tr>
+                    <tr>
+                        <td class='table-light'>Latitude</td>
+                        <td> {{ $btslist->latitude }} </td>
+                    </tr>
+                    <tr >
+                        <td class='table-light' >Longitude</td>
+                        <td> {{ $btslist->longitude }} </td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     </div>
 </div>
