7ec083e69f90f626e7f4643d0230a61283351a83
bytadit <bgsp_aditya@student.uns.ac.id>
2022-07-03 13:28:14 +0700

    add handler, survey gone if bts object not exists

--- Apl Laravel/monitorBts/resources/views/dashboard/admin/survey/index.blade.php
+++ Apl Laravel/monitorBts/resources/views/dashboard/admin/survey/index.blade.php
@@ -11,18 +11,28 @@
     @endif
     <div class="d-flex flex-wrap survey-cards" style="justify-content:space-evenly;">
         @foreach ($surveys as $key=>$survey)
-        <div class="card text-white bg-bts-3 border-3 survey-card" style="width: 18rem; margin:10px">
-            <h3 class="card-header">{{ $survey->name }}</h3>
-            <div class="card-body scroll">
-                <p class="card-text">{{ $survey->description }}</p>
-            </div>
-            <p class='card-header text-end text-light'><span class='fw-bolder text-light'>{{$survey->question->count()}}</span> pertanyaan</p>
-            <form action="/dashboard/surveys/{{ $survey->id }}" method="post" class="card-footer text-center ">
-                @method('delete')
-                @csrf
-                <button class="badge bg-danger border-0 " title="Delete" data-toggle="tooltip" onclick="return confirm('Are you sure?')"><i class="bi bi-trash-fill"></i></button>
-            </form>
-        </div>
+            @if ($survey->btslists->count() != 0)
+                <div class="card text-white bg-bts-3 border-3 survey-card" style="width: 18rem; margin:10px">
+                    <h3 class="card-header">{{ $survey->name }}</h3>
+                    <div class="card-body scroll">
+                        <p class="card-text">{{ $survey->description }}</p>
+                    </div>
+                    <p class='card-header text-end text-light'><span class='fw-bolder text-light'>{{$survey->question->count()}}</span> pertanyaan</p>
+                    <p class='card-header text-first text-light'>
+                    <span class='fw-bolder text-light text-first'>Objek BTS : </span><br>
+                        <span class='text-light'>
+                            @foreach ($survey->btslists as $btslist)
+                                {{$btslist->nama . '; '}}
+                            @endforeach
+                        </span>
+                    </p>
+                    <form action="/dashboard/surveys/{{ $survey->id }}" method="post" class="card-footer text-center ">
+                        @method('delete')
+                        @csrf
+                        <button class="badge bg-danger border-0 " title="Delete" data-toggle="tooltip" onclick="return confirm('Are you sure?')"><i class="bi bi-trash-fill"></i></button>
+                    </form>
+                </div>
+            @endif
         @endforeach
         <div class="card text-black bg-primary text-white border-3 add-card survey-card" style="width: 18rem; margin:10px">
             <h3 class="card-header">Tambah Survey</h3>
